e8718f879c4ef032d146e518fa20b7ae4f426320
Hunter Caron <huntercaron@mac.com>
2018-12-08 17:24:14 -0800

    fixing build

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,7 +61,7 @@ exports.createPages = ({ graphql, actions }) => {
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  
+
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
@@ -72,4 +72,20 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 
   fmImagesToRelative(node)
+}
+
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /intersection-observer/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
 }
