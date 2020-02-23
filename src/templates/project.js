3474c928cab9436c2d685c0cfedbccb98bcb740d
Hunter Caron <huntercaron@me.com>
2020-02-23 14:18:24 +0100

    refactoring to hook

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-import Img from "gatsby-image"
-import styled from 'styled-components';
-import "intersection-observer"
+import Img from 'gatsby-image'
+import styled from 'styled-components'
+import 'intersection-observer'
 
 const Title = styled.h1`
-    text-align: left;
-    font-size: 1.35rem;
-    letter-spacing: 0.2px;
-    transition: all 500ms ease;
-    font-family: Georgia;
-    text-transform: uppercase;
-    font-size: 1.1rem;
-    letter-spacing: 0.7px;
-    margin-bottom: 2rem;
-    line-height: 1.4;
-    width: 100%;
-    max-width: 1100px;
-`;
+  text-align: left;
+  font-size: 1.35rem;
+  letter-spacing: 0.2px;
+  transition: all 500ms ease;
+  font-family: Georgia;
+  text-transform: uppercase;
+  font-size: 1.1rem;
+  letter-spacing: 0.7px;
+  margin-bottom: 2rem;
+  line-height: 1.4;
+  width: 100%;
+  max-width: 1100px;
+`
 
 const Info = styled.div`
   width: 100%;
@@ -34,7 +34,7 @@ const Description = styled.div`
   margin-bottom: 4rem;
   margin-right: 2rem;
   width: 100%;
-`;
+`
 
 const Container = styled.div`
   width: 100%;
@@ -44,7 +44,7 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-`;
+`
 
 const Gallery = styled.div`
   width: 100%;
@@ -57,45 +57,52 @@ const Gallery = styled.div`
     width: 100%;
     margin: 2rem 1rem;
     max-width: 1100px;
-    max-height: calc(100vh - 4rem); 
-    
+    max-height: calc(100vh - 4rem);
+
     img {
       object-fit: contain !important;
     }
   }
-`;
+`
 
-class ProjectTemplate extends React.Component {
-  render() {
-    const project = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const siteDescription = project.excerpt
+const ProjectTemplate = ({ data }) => {
+  const project = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const siteDescription = project.excerpt
 
-    // console.log(this.props)
+  return (
+    <div>
+      <Container>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={`${project.frontmatter.title} | ${siteTitle}`}
+        />
 
-    return (
-      <div>
-        <Container>
-          <Helmet
-            htmlAttributes={{ lang: 'en' }}
-            meta={[{ name: 'description', content: siteDescription }]}
-            title={`${project.frontmatter.title} | ${siteTitle}`}
+        <Info>
+          <Title>
+            <span className="underline underline-black">______ </span>{' '}
+            {project.frontmatter.title}
+          </Title>
+          <Description
+            dangerouslySetInnerHTML={{
+              __html: project.frontmatter.description,
+            }}
           />
+        </Info>
 
-          <Info>
-            <Title><span className="underline underline-black">______ </span> {project.frontmatter.title}</Title>
-            <Description dangerouslySetInnerHTML={{ __html: project.frontmatter.description }} />
-          </Info>
-        
-          <Gallery>
-            {project.frontmatter.gallery_images.map(image =>
-              <Img backgroundColor="#fbfbfb" key={image.id} fluid={image.childImageSharp.fluid}/>
-            )}
-          </Gallery>
-        </Container>
-      </div>
-    )
-  }
+        <Gallery>
+          {project.frontmatter.gallery_images.map(image => (
+            <Img
+              backgroundColor="#fbfbfb"
+              key={image.id}
+              fluid={image.childImageSharp.fluid}
+            />
+          ))}
+        </Gallery>
+      </Container>
+    </div>
+  )
 }
 
 export default ProjectTemplate
@@ -108,12 +115,20 @@ export const pageQuery = graphql`
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      ...TinaRemark
       id
       excerpt
       html
       frontmatter {
         title
         description
+        thumbnail {
+          childImageSharp {
+            fluid(maxWidth: 1100, quality: 90) {
+              ...GatsbyImageSharpFluid_withWebp_noBase64
+            }
+          }
+        }
         gallery_images {
           id
           childImageSharp {
